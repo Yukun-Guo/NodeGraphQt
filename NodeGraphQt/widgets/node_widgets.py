1d3d0170bedadf71240d52b0f09cff9a949bb420
Yukun <guoyukn@gmail.com>
2025-12-09 13:53:22 -0800

    Increase title text color opacity in _NodeGroupBox for better visibility

--- NodeGraphQt/widgets/node_widgets.py
+++ NodeGraphQt/widgets/node_widgets.py
@@ -34,7 +34,7 @@ class _NodeGroupBox(QtWidgets.QGroupBox):
             'QGroupBox::title': {
                 'subcontrol-origin': 'margin',
                 'subcontrol-position': 'top center',
-                'color': 'rgba({0}, {1}, {2}, 100)'.format(*text_color),
+                'color': 'rgba({0}, {1}, {2}, 230)'.format(*text_color),
                 'padding': '0px',
             }
         }
